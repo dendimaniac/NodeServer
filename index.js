2568a3330ee39e2e6332925e47aab207063760df
Dendi Maniac <daq30899@gmail.com>
2019-11-15 14:31:35 +0200

    database connection in separated file

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 'use strict';
 
 const express = require('express');
+const connection = require('./model/db');
 
 const app = express();
 
 app.use(express.static('public'));
+
+app.get('/animal', async (req, res) => {
+    try {
+        const [results, fields] = await connection.query('SELECT * FROM animal');
+        console.log(results); // results contains rows returned by server
+        console.log(fields); // fields contains extra meta data about results, if available
+        res.json(results);
+    } catch (e) {
+        console.log(e);
+        res.send('db error :(');
+    }
+})
+;
 
 app.get('/', (req, res) => {
     res.send('Hello from my Node Server');
