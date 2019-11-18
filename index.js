2866b73d2210844470b217e904a8515ba1789776
Dendi Maniac <daq30899@gmail.com>
2019-11-18 14:24:53 +0200

    body parser with HTTP post and mysql method

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+//body parser with HTTP post and mysql method
 'use strict';
 
 const express = require('express');
 const connection = require('./model/db');
 
 const app = express();
+const bodyParser = require('body-parser');
 
 app.use(express.static('public'));
 
@@ -22,13 +24,27 @@ app.get('/animals', async (req, res) => {
 app.get('/animal', async (req, res) => {
     console.log(req.query);
     try {
-        const [results] = await  connection.query(
+        const [results] = await connection.query(
             'SELECT * FROM animal WHERE name LIKE ?',
             [req.query.name]
         );
         res.json(results);
     } catch (e) {
         res.send(`db error ${e}`);
+    }
+});
+
+app.post('/animal', bodyParser.urlencoded({extended: true}), async (req, res) => {
+    console.log(req.body);
+    try {
+        const [result] = await connection.query(
+            'INSERT INTO animal (name) VALUES (?)',
+            [req.body.name]
+        );
+        res.json(result);
+    } catch (e) {
+        console.log(e);
+        res.send('db error');
     }
 });
 
