1c3365e7d203dda2e6bb633d95eb83b849edb39f
Dendi Maniac <daq30899@gmail.com>
2019-11-18 12:55:16 +0200

    prepare statement for SQL and HTTP GET URL query parameters

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(express.static('public'));
 
-app.get('/animal', async (req, res) => {
+app.get('/animals', async (req, res) => {
     try {
         const [results, fields] = await connection.query('SELECT * FROM animal');
         console.log(results); // results contains rows returned by server
@@ -16,6 +16,19 @@ app.get('/animal', async (req, res) => {
     } catch (e) {
         console.log(e);
         res.send('db error :(');
+    }
+});
+
+app.get('/animal', async (req, res) => {
+    console.log(req.query);
+    try {
+        const [results] = await  connection.query(
+            'SELECT * FROM animal WHERE name LIKE ?',
+            [req.query.name]
+        );
+        res.json(results);
+    } catch (e) {
+        res.send(`db error ${e}`);
     }
 });
 
